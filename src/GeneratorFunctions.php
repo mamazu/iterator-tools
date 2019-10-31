542275fb603809377cccc87ec09deeef49f1e46e
mamazu <14860264+mamazu@users.noreply.github.com>
2019-10-31 11:21:12 +0100

    Initial commit

---
+++ src/GeneratorFunctions.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+class IteratorTools {
+    public function iterator_filter(iterable $iterable, callable $func): iterable {
+        foreach($iterable as $item) {
+            if ($func($item)) {
+                yield $item;
+            }
+        }
+    }
+
+    public function iterator_map(iterable $iterable, callable $func): iterable {
+        foreach($iterable as $item) {
+            yield $func($item);
+        }
+    }
+
+    public function iterator_reduce(iterable $iterable, callable $func, $startValue = null) {
+        $value = $startValue;
+        foreach($iterable as $item) {
+            $value = $func($value, $item);
+        }
+        return $value;
+    }
+
+    public function create_from_array(array $data): iterable
+    {
+        yield from $data;
+    }
+
+    public function create_from_filename(string $fileName): iterable
+    {
+        return $this->create_from_file_handle(fopen($fileName, 'r'));
+    }
+
+    public function create_from_file_handle(resource $handle): iterable
+    {
+        while (($buffer = fgets($handle)) !== false) {
+            yield $buffer;
+        }
+    }
+}
