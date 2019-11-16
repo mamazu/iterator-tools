5fcbc170967aa8d6d7b4f80bf55729aa38b75a6b
mamazu <14860264+mamazu@users.noreply.github.com>
2019-11-16 13:19:14 +0100

    Moving to direct functions

--- src/IteratorTools.php
+++ src/IteratorTools.php
@@ -4,43 +4,43 @@ declare(strict_types=1);
 
 namespace Mamazu\IteratorTools;
 
-class IteratorTools {
-    public function iterator_filter(iterable $iterable, callable $func): iterable {
-        foreach($iterable as $item) {
-            if ($func($item)) {
-                yield $item;
-            }
+use iterable;
+
+function iterator_filter(iterable $iterable, callable $func): iterable
+{
+    foreach ($iterable as $item) {
+        if ($func($item)) {
+            yield $item;
         }
     }
+}
 
-    public function iterator_map(iterable $iterable, callable $func): iterable {
-        foreach($iterable as $item) {
-            yield $func($item);
-        }
+function iterator_map(iterable $iterable, callable $func): iterable
+{
+    foreach ($iterable as $item) {
+        yield $func($item);
+    }
+}
+
+function iterator_reduce(iterable $iterable, callable $func, $startValue = null)
+{
+    $value = $startValue;
+    foreach ($iterable as $item) {
+        $value = $func($value, $item);
     }
 
-    public function iterator_reduce(iterable $iterable, callable $func, $startValue = null) {
-        $value = $startValue;
-        foreach($iterable as $item) {
-            $value = $func($value, $item);
-        }
-        return $value;
-    }
+    return $value;
+}
 
-    public function create_from_array(array $data): iterable
-    {
-        yield from $data;
-    }
+function array_to_iterator(array $data): iterable
+{
+    yield from $data;
+}
 
-    public function create_from_filename(string $fileName): iterable
-    {
-        return $this->create_from_file_handle(fopen($fileName, 'r'));
-    }
-
-    public function create_from_file_handle(resource $handle): iterable
-    {
-        while (($buffer = fgets($handle)) !== false) {
-            yield $buffer;
-        }
+function file(string $fileName): iterable
+{
+    $handle = fopen($fileName, 'rb');
+    while (($line = fgets($handle)) !== false) {
+        yield $line;
     }
 }
--- tests/IteratorsTest.php
+++ tests/IteratorsTest.php
@@ -4,65 +4,69 @@ declare(strict_types=1);
 
 namespace tests\Mamazu\IteratorTools;
 
+use iterable;
 use PHPUnit\Framework\TestCase;
-use Mamazu\IteratorTools\IteratorTools;
+use function Mamazu\IteratorTools\array_to_iterator;
+use function Mamazu\IteratorTools\iterator_filter;
+use function Mamazu\IteratorTools\iterator_map;
+use function Mamazu\IteratorTools\iterator_reduce;
 
-class GeneratorFunctionsTest extends TestCase {
+class IteratorsTest extends TestCase {
     public function test_iterator_filter(): void {
-        $generator = function () { yield from [1, 2, 3, 4, 5, 6, 7, 8, 9]; };
+        $generator = static function () { yield from [1, 2, 3, 4, 5, 6, 7, 8, 9]; };
 
-        $result = IteratorTools::iterator_filter($generator(), function ($x) {return $x % 2 === 0; } );
+        $result = iterator_filter($generator(), static function ($x) {return $x % 2 === 0; } );
         $array_result = $this->unroll($result);
         $this->assertCount(4, $array_result);
         $this->assertSame($array_result, [2, 4, 6, 8]);
     }
 
     public function test_iterator_filter_with_empty_result(): void {
-        $generator = function () { yield from [1, 2, 3, 4, 5, 6, 7, 8, 9]; };
+        $generator = static function () { yield from [1, 2, 3, 4, 5, 6, 7, 8, 9]; };
 
-        $result = IteratorTools::iterator_filter($generator(), function ($x) {return $x > 10; } );
+        $result = iterator_filter($generator(), static function ($x) {return $x > 10; } );
         $array_result = $this->unroll($result);
         $this->assertCount(0, $array_result);
         $this->assertSame($array_result, []);
     }
 
     public function test_iterator_map(): void {
-        $generator = function () { yield from [1, 2, 3, 4, 5, 6, 7, 8, 9]; };
+        $generator = static function () { yield from [1, 2, 3, 4, 5, 6, 7, 8, 9]; };
 
-        $result = IteratorTools::iterator_map($generator(), function ($x) { return $x * 2; });
-        
+        $result = iterator_map($generator(), static function ($x) { return $x * 2; });
+
         $array_result = $this->unroll($result);
         $this->assertCount(9, $array_result);
         $this->assertSame($array_result, [2, 4, 6, 8, 10, 12, 14, 16, 18]);
     }
 
     public function test_iterator_reduce(): void {
-        $generator = function () { yield from [1, 2, 3, 4, 5, 6, 7, 8, 9]; };
+        $generator = static function () { yield from [1, 2, 3, 4, 5, 6, 7, 8, 9]; };
 
-        $result = IteratorTools::iterator_reduce(
-            $generator(), 
-            function (int $carry, int $item) { return $carry + $item; },
+        $result = iterator_reduce(
+            $generator(),
+            static function (int $carry, int $item) { return $carry + $item; },
             0
         );
         $this->assertSame($result, 45);
     }
-    
-    public function test_iterator_reduce_with_null(): void {
-        $generator = function () { yield from [1, 2, 3, 4, 5, 6, 7, 8, 9]; };
 
-        $result = IteratorTools::iterator_reduce(
-            $generator(), 
-            function (?int $carry, int $item) { return ($carry ?? 0) + $item; }
+    public function test_iterator_reduce_with_null(): void {
+        $generator = static function () { yield from [1, 2, 3, 4, 5, 6, 7, 8, 9]; };
+
+        $result = iterator_reduce(
+            $generator(),
+            static function (?int $carry, int $item) { return ($carry ?? 0) + $item; }
         );
         $this->assertSame($result, 45);
     }
-    
+
     public function test_iterator_from_array(): void {
-        $result = IteratorTools::create_from_array([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        $result = array_to_iterator([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
         $this->assertEquals($this->unroll($result), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
     }
-    
+
     private function unroll(iterable $iterable): array {
         $array = [];
         foreach($iterable as $item) {
